d97a9eac7a98763674197963b5a14b3d78cbd022
Rob <robscafe333@yahoo.com>
2024-10-13 23:00:48 -0700

    Practicing object functions vs object literals, saved file this time

---
+++ 02-Constructors/index.js
@@ -0,0 +1,24 @@
+// TODO: Create a dog object from scratch
+
+const dog = {
+    name: 'Rex',
+    age: 2,
+    breed: 'husky'
+    }
+    
+    
+    // TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 'text, and 'createdOn'
+    
+    function Cat (name, age, breed){
+    
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        
+    }
+    // TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
+    
+    // TODO: Create a new object using the 'BlogPost' constructor
+    
+    // TODO: Call the 'printMetaData()' method on the new object
+    
