fa7e7ae1bfa1684b7a14c5a03024e3dc1da4a475
Rob <robscafe333@yahoo.com>
2024-10-14 10:51:54 -0700

    completed todo's , continuation of contructor functions

--- 02-Constructors/index.js
+++ 02-Constructors/index.js
@@ -7,18 +7,31 @@ const dog = {
     }
     
     
-    // TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 'text, and 'createdOn'
+    // TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 
+    // 'text, and 'createdOn'
     
-    function Cat (name, age, breed){
+    function BlogPost (authorName, title, text, createdOn){
     
-        this.name = name;
-        this.age = age;
-        this.breed = breed;
+        this.authorName = authorName
+        this.title = title;
+        this.text = text;
+        this.createdOn = createdOn
+        this.printMetaData = function () {
+            console.log(`Created by ${this.authorName} on ${this.createdOn}`);
+          };
         
     }
     // TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
+
     
     // TODO: Create a new object using the 'BlogPost' constructor
+
+    const post = new BlogPost(
+        "Charles Dickens",
+        "Great Expectations",
+        "good book",
+        '10-13-24'
+    )
     
     // TODO: Call the 'printMetaData()' method on the new object
-    
+   post.printMetaData()
