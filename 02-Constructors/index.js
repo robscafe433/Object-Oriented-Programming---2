0140cc0f1156123df56cdc545bb9b7c2fb4cf8b2
Rob <robscafe333@yahoo.com>
2024-10-15 23:41:07 -0700

    more practice with constructer functions, bugs i encountered, had two variables with same name

--- 02-Constructors/index.js
+++ 02-Constructors/index.js
@@ -1,37 +1,50 @@
 // TODO: Create a dog object from scratch
 
 const dog = {
-    name: 'Rex',
-    age: 2,
-    breed: 'husky'
-    }
-    
-    
-    // TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 
-    // 'text, and 'createdOn'
-    
-    function BlogPost (authorName, title, text, createdOn){
-    
-        this.authorName = authorName
-        this.title = title;
-        this.text = text;
-        this.createdOn = createdOn
-        this.printMetaData = function () {
-            console.log(`Created by ${this.authorName} on ${this.createdOn}`);
-          };
-        
-    }
-    // TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
+	name: "Rex",
+	age: 2,
+	breed: "husky",
+};
 
-    
-    // TODO: Create a new object using the 'BlogPost' constructor
+// TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title',
+// 'text, and 'createdOn'
 
-    const post = new BlogPost(
-        "Charles Dickens",
-        "Great Expectations",
-        "good book",
-        '10-13-24'
-    )
-    
-    // TODO: Call the 'printMetaData()' method on the new object
-   post.printMetaData()
+function BlogPost(authorName, title, text, createdOn) {
+	this.authorName = authorName;
+	this.title = title;
+	this.text = text;
+	this.createdOn = createdOn;
+	this.printMetaData = function () {
+		console.log(`Created by ${this.authorName} on ${this.createdOn}`);
+	};
+}
+// TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
+
+// TODO: Create a new object using the 'BlogPost' constructor
+
+const post = new BlogPost(
+	"Charles Dickens",
+	"Great Expectations",
+	"good book",
+	"10-13-24"
+);
+
+// TODO: Call the 'printMetaData()' method on the new object
+post.printMetaData();
+
+function DogPost(breed, age) {
+	this.breed = breed;
+	this.age = age;
+	this.printDogData = function () {
+		console.log(
+			"This is the dog data, breed: ",
+			this.breed,
+			"and age:",
+			this.age
+		);
+	};
+}
+
+const myDog = new DogPost("Husky", 5);
+
+myDog.printDogData();
